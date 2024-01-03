b553983b8a4246a3af21446508dfe309de8d02ba
Jacob <jacob@Jacobs-Mac-Studio.local>
2024-01-02 17:04:14 -0800

    Refactored file structure

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Form } from "react-router-dom";
 import "./styles/SearchBar.css"
+import mag from "./images/magnifying_glass.png";
 
 function SearchBar() {
     return <Form className="search-form" action="submit">
         <input className="search-bar" type="search" name="search" placeholder="Search..."></input>
         <button className="search-button">
-            <img src="../images/magnifying_glass.png" alt="search button"/>
+            <img src={mag} alt="search button"/>
         </button>
     </Form>
 }
